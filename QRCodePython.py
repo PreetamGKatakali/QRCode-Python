f49d61dd958714ce483cf61301281dbdd64ec063
PreetamGKatakali <preetam19cs051@gmail.com>
2022-09-28 11:54:37 +0530

    added

---
+++ QRCodePython.py
@@ -0,0 +1,7 @@
+from turtle import back, fillcolor
+import qrcode
+feature=qrcode.QRCode(box_size=40,border=3)   #size of QRCOde 
+feature.add_data("https://www.google.com/")   #link appers when QRCode scans
+# feature.make(fit=True)
+generate_image=feature.make_image(fill_color="black",back_color="white")   #color of QRCode 
+generate_image.save("image.png")   #name of the image should be saved
