b92460b559b289da998fd1c8f32f49b7bd4aebc9
PreetamGKatakali <preetam19cs051@gmail.com>
2022-09-28 12:00:37 +0530

    added

--- QRCodePython.py
+++ QRCodePython.py
@@ -1,6 +1,6 @@
 from turtle import back, fillcolor
 import qrcode
-feature=qrcode.QRCode(box_size=10,border=2)   #size of QRCOde 
+feature=qrcode.QRCode(box_size=5,border=1)   #size of QRCOde 
 feature.add_data("https://www.google.com/")   #link appers when QRCode scans
 # feature.make(fit=True)
 generate_image=feature.make_image(fill_color="black",back_color="white")   #color of QRCode 
